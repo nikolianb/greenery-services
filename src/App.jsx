e802cb88fd6a99475d884cb968aaf63dfa84d360
Nikolian <nikolian@gmx.com>
2026-03-25 22:28:57 +0100

    seo

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Services from './components/Services';
 import About from './components/About';
-import HowItWorks from './components/Gallery';
+import HowItWorks from './components/HowItWorks';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
